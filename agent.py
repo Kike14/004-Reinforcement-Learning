0d625df9f905c51504c58328c0f0b7e0488df9a8
Majo Elizalde <if725710@iteso.mx>
2024-11-26 18:34:04 -0600

    Commit Majo

--- agent.py
+++ agent.py
@@ -35,3 +35,53 @@ class Agent:
             tf.keras.layers.Dense(self.action_space, activation='linear')
         ])
         return model
+
+    def train_step(self):
+        if len(self.replay_buffer) < self.batch_size:
+            return
+
+        states, actions, rewards, next_states, dones = self.get_sample()
+        next_q_values = self.q_target_network.predict(next_states, verbose=0)
+        max_next_q_values = np.max(next_q_values, axis=1)
+        target_q_values = rewards + (1 - dones) * self.gamma * max_next_q_values
+
+        with tf.GradientTape() as tape:
+            q_values = self.q_network(states, training=True)
+            actions_one_hot = tf.one_hot(actions, self.action_space)
+            q_values_for_actions = tf.reduce_sum(q_values * actions_one_hot, axis=1)
+            loss = self.loss(target_q_values, q_values_for_actions)
+
+        grads = tape.gradient(loss, self.q_network.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.q_network.trainable_variables))
+
+    def train(self):
+        for epoch in range(self.max_epochs):
+            state = self.env.reset()
+            total_reward = 0
+            for step in range(self.max_steps):
+                if np.random.rand() < self.epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    state_tensor = tf.convert_to_tensor(state[None, :], dtype=tf.float32)
+                    q_values = self.q_network(state_tensor, training=False)
+                    action = np.argmax(q_values.numpy())
+
+                next_state, reward, done, _ = self.env.step(action)
+                self.replay_buffer.append((state, action, reward, next_state, done))
+                state = next_state
+                total_reward += reward
+
+                self.train_step()
+
+                if done:
+                    break
+
+            self.epsilon = max(self.epsilon_min, self.epsilon * 0.99)
+            print(f"Epoch: {epoch + 1}, Total Reward: {total_reward}")
+
+    def save_model(self, path):
+        self.q_network.save(path)
+
+    def load_model(self, path):
+        self.q_network = tf.keras.models.load_model(path)
+        self.q_target_network.set_weights(self.q_network.get_weights())
